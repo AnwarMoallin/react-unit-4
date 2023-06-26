191a36e4b86de3e09124d9a5a8a3c8d4694baa93
anwarice <amoa1701@gmail.com>
2023-06-25 23:23:28 -0500

    completed react-unit-4

---
+++ src/components/Auth.js
@@ -0,0 +1,81 @@
+import { useState, useContext } from "react";
+import axios from "axios";
+
+import AuthContext from "../store/authContext";
+import { Navigate, useNavigate } from "react-router-dom";
+
+const Auth = () => {
+  const navigate = useNavigate();
+  const [register, setRegister] = useState(true);
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [message, setMessage] = useState("");
+  const [display, setDisplay] = useState("none");
+
+  const authCtx = useContext(AuthContext);
+
+  const submitHandler = (e) => {
+    if (!username || !password) {
+      alert("Please fill the fields");
+      return;
+    }
+
+    navigate(`/`);
+
+    e.preventDefault();
+
+    setDisplay("none");
+
+    const body = {
+      username,
+      password,
+    };
+
+    const url = "https://socialmtn.devmountain.com";
+
+    axios
+      .post(register ? `${url}/register` : `${url}/login`, body)
+      .then((res) => {
+        console.log("AFTER AUTH", res.data);
+        authCtx.login(res.data.token, res.data.exp, res.data.userId);
+      })
+      .catch((err) => {
+        setMessage(err.response.data);
+        setDisplay("block");
+        setPassword("");
+        setUsername("");
+      });
+    navigator(`/`);
+  };
+
+  return (
+    <main>
+      <h1>Welcome!</h1>
+      <form className="form auth-form" onSubmit={submitHandler}>
+        <input
+          type="text"
+          placeholder="username"
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
+          className="form-input"
+        />
+        <input
+          type="password"
+          placeholder="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          className="form-input"
+        />
+        <button className="form-btn">{register ? "Sign Up" : "Login"}</button>
+      </form>
+      <p style={{ display: display }} className="auth-msg">
+        {message}
+      </p>
+      <button className="form-btn" onClick={() => setRegister(!register)}>
+        Need to {register ? "Login" : "Sign Up"}?
+      </button>
+    </main>
+  );
+};
+
+export default Auth;
